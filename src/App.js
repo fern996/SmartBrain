3439a61bbab54cc7668fad8e58a526c036dc3896
sf0912 <shawnfernandes0912@gmail.com>
2022-11-30 17:22:57 -0500

    Added Signin and Register functionality, tested with backend compoenent.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn'
+import Register from './components/Register/Register'
 import ParticlesBg from 'particles-bg';
 import Clarifai from 'clarifai'
 import './App.css';
@@ -15,10 +17,55 @@ const app = new Clarifai.App({
 class App extends Component{
   constructor(){
     super();
-    this.state ={
+    this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
+      route:'signin',
+      isSignedIn: false,
+      user: {
+        id: '123',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState( {user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
+  // componentDidMount(){
+  //   fetch('http://localhost:3000')
+  //     .then(response => response.json())
+  //     .then(console.log)
+  // }
+
+  calculateFaceLocation = (data) => {
+    const claraifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: claraifaiFace.left_col * width,
+      topRow: claraifaiFace.top_row * height,
+      rightCol: width - (claraifaiFace.rightcol * width),
+      bottomRow: height - (claraifaiFace.bottom_row * height)
+    }
+  }
+
+
+
+  displayFaceBox = (box) => {
+    this.setState({box:box});
   }
 
   onInputChange = (event) =>{
@@ -28,17 +75,13 @@ class App extends Component{
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
     app.models
-    .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
+    .predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input
+      )
 
-    .then(
-    function(response){
-      //do something
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err){
-      //there was an error
-    }
-  );
+    .then(response =>this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
   }
 
 
@@ -78,21 +121,42 @@ class App extends Component{
  //      })
  //      .catch(err => console.log(err));
  // }
-
+  onRouteChange = (route) => {
+    if(route === 'signout'){
+      this.setState({isSignedIn: false})
+    } else if (route === 'home'){
+      this.setState({isSignedIn:true})
+    }
+    this.setState({route:route})
+  }
 
   render(){
+    const { isSignedIn, imageUrl, route, box} = this.state
     return(
     <div className="App">
     <>
         <ParticlesBg type="cobweb" bg={true} color="#FFFFFF" />
     </>
-      <Navigation />
-          <Logo />
-          <Rank/>
-          <ImageLinkForm onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-          />
-          <FaceRecognition imageUrl={this.state.imageUrl}/>
+      <Navigation isSignedIn = {isSignedIn} onRouteChange={this.onRouteChange}/>
+        { this.state.route === 'home' ?
+            <div>
+              <Logo />
+              <Rank/>
+              <ImageLinkForm 
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition 
+              box={box}
+              imageUrl={imageUrl}
+              />
+            </div>
+            : (
+              route === 'signin'
+              ? <SignIn onRouteChange={this.onRouteChange}/>
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+              )
+        }
     </div>
     );
   }
